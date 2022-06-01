b08dccd6276ad355bca173eb5acf71b5a0de9cce
Maximiliano Herrera <Maximiliano>
2022-05-31 21:51:21 -0300

    crear un algoritmo creando un ciclo

--- js/variables.js
+++ js/variables.js
@@ -1,6 +1,41 @@
 //Inicio
 
-let nombre = prompt("Ingrese su nombre:")
+let nombre = prompt("Ingresa tu nombre: ");
+let apellido = prompt("Ingresa tu apellido: ");
+
+debugger
+
+let ingresarNumero = parseInt(prompt("Ingresar Numero"));
+let resultado;
+let numero;
+   
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = ingresarNumero + i ;
+    alert(ingresarNumero +" + "+ i +" = "+ resultado);
+
+    if (resultado > 0 || resultado < 5) {
+    break;}
+        numero = parseInt(prompt("Ingresa un número: "));}
+
+    
+    
+    let entrada = prompt("Ingresar un equipo de futbol");
+
+    while (entrada != "ESC" ){
+    alert("El usuario ingresó "+ entrada);
+    
+        if (entrada == river) {
+            continue;}
+        entrada = prompt("Ingresar un equipo de primera no de la B");}
+    
+
+
+    
+    
+
+
+
 
 
 
