54df95ac3b68ac220032b38d73e0f17f2d010410
Maximiliano Herrera <Maximiliano>
2022-06-09 21:20:06 -0300

    se modifico nav

--- js/variables.js
+++ js/variables.js
@@ -1,18 +1,18 @@
 //Inicio
 
-let nombre = prompt("Ingresa tu nombre: ");
-let apellido = prompt("Ingresa tu apellido: ");
+// let nombre = prompt("Ingresa tu nombre: ");
+// let apellido = prompt("Ingresa tu apellido: ");
 
 //Ingrese su edad mayor a 18.
 
-let edad = prompt("Ingresar su edad");
+// let edad = prompt("Ingresar su edad");
 
-while(edad >= "18" ){
-    alert("Su edad ingresada "+ edad);
+// while(edad >= "18" ){
+//     alert("Su edad ingresada "+ edad);
     
-    edad = prompt("Ingresar nuevamente su edad");
+//     edad = prompt("Ingresar nuevamente su edad");
     
-}
+// }
 
 
 
